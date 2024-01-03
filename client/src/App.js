5c2f3d3ef0c6048e289d2d1dbafa241a4c8db37b
chimbo87 <archfordnhanga@gmail.com>
2024-01-03 21:28:39 +0200

    frontend designs

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/header/Navbar";
-
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-  <Navbar/>
+      <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,93 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <div id="ourWorkPage">
+        <div id="aaa">
+          <div id="homeTexts">
+            <h1>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has
+            </h1>
+            <button>Lets Chat</button>
+          </div>
+        </div>
+        <div class="triangle"></div>
+      </div>
+      <div className="container" id="homeContentBox">
+        <div>
+          <h4>What We Do</h4>
+          <div class="row">
+            <div class="col-lg-4 col-md-4">
+              <div id="homeOfferCard">
+                <i class="bx bxs-devices"></i>
+                <h5>Phone Repair</h5>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum
+                </p>
+                <div>
+                  <button>Read More</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <div id="homeOfferCard">
+                <i class="bx bxs-devices"></i>
+                <h5>Phone Repair</h5>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum
+                </p>
+                <div>
+                  <button>Read More</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <div id="homeOfferCard">
+                <i class="bx bxs-devices"></i>
+                <h5>Sell Phone Accessories</h5>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum
+                </p>
+                <div>
+                  <button>Read More</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
